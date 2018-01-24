4225b7f214ba38bf00227a15a54179b99cc9b9e7
boniface <boniface@kabaso.com>
2018-01-24 20:16:12 +0200

    Updated

--- app/src/main/java/za/ac/cput/inforshareapp/domain/users/User.java
+++ app/src/main/java/za/ac/cput/inforshareapp/domain/users/User.java
@@ -1,8 +1,31 @@
 package za.ac.cput.inforshareapp.domain.users;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /**
  * Created by hashcode on 2018/01/24.
  */
 
-public class User {
+public class User implements Serializable {
+
+    private String siteId;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String screenName;
+    private String password;
+    private String state;
+    private LocalDateTime date;
+
+    public static class Builder{
+        private String siteId;
+        private String email;
+        private String firstName;
+        private String lastName;
+        private String screenName;
+        private String password;
+        private String state;
+        private LocalDateTime date;
+    }
 }
